656838ff377ebc7cac4f180cede99ceb8c61a422
Kevin Hardeman <mail@kevinhardeman.com>
2024-06-05 21:52:03 +0200

    eerste opzet kale MVP

--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,34 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+return (
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>ScootService</title>
+  <link rel="stylesheet" href="style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+  <div class="container">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+        <img src="/images/Logo.jpeg" width="60" height="60">
+        <span class="fs-4">ScootService - scootmobielverhuur op maat</span>
+      </a>
+    </header>
+  </div>
 
-      <Footer />
-    </div>
-  )
+  <div class="bd-example m-0 border-0">
+    <img src="/images/Lake.jpg" class="img-fluid rounded mx-auto d-block" alt="Responsive image">
+
+  </div>
+
+</body>
+</html>
+)
 }
