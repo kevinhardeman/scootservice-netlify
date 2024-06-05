117caa75ec2dc3d0761f6e25651a285e578eb2dc
Kevin Hardeman <mail@kevinhardeman.com>
2024-06-05 22:17:31 +0200

    eerste opzet kale MVP

--- pages/index.js
+++ pages/index.js
@@ -13,19 +13,17 @@ return (
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></link>
   </head>
   <body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
     <div class="container">
       <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
         <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-          <img src="/Logo.jpeg" width="60" height="60"></img>
+          <img src="/images/Logo.jpeg" width="60" height="60"></img>
           <span class="fs-4">ScootService - scootmobielverhuur op maat</span>
         </a>
       </header>
     </div>
 
     <div class="bd-example m-0 border-0">
-      <img src="/Lake.jpg" class="img-fluid rounded mx-auto d-block" alt="Responsive image"></img>
+      <img src="/images/Lake.jpg" class="img-fluid rounded mx-auto d-block" alt="Responsive image"></img>
 
     </div>
 
