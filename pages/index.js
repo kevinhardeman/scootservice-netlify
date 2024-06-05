46c6367b589d7b1094165d8ac9dff7bb42cac860
Kevin Hardeman <mail@kevinhardeman.com>
2024-06-05 22:02:21 +0200

    eerste opzet kale MVP

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,8 @@ export default function Home() {
 return (
   <html lang="en">
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8"></meta>
+    <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
     <title>ScootService</title>
     <link rel="stylesheet" href="style.css"></link>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></link>
@@ -18,14 +18,14 @@ return (
     <div class="container">
       <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
         <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-          <img src="/images/Logo.jpeg" width="60" height="60">
+          <img src="/images/Logo.jpeg" width="60" height="60"></img>
           <span class="fs-4">ScootService - scootmobielverhuur op maat</span>
         </a>
       </header>
     </div>
 
     <div class="bd-example m-0 border-0">
-      <img src="/images/Lake.jpg" class="img-fluid rounded mx-auto d-block" alt="Responsive image">
+      <img src="/images/Lake.jpg" class="img-fluid rounded mx-auto d-block" alt="Responsive image"></img>
 
     </div>
 
